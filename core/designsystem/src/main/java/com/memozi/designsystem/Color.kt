185123d61feb452a7f3f96f0202d791d2b135e2b
김명석 <twinaudtjr@naver.com>
2024-08-27 05:09:40 +0900

    [add] #1 Color.kt

---
+++ core/designsystem/src/main/java/com/memozi/designsystem/Color.kt
@@ -0,0 +1,42 @@
+package com.memozi.designsystem
+
+import androidx.compose.ui.graphics.Color
+// Main
+val MainPink = Color(0xFFE3ADB2)
+val MainPurple = Color(0xFFBFBCE7)
+
+// Action Button Color
+val MainPurple02 = Color(0xB2BFBCE7) // 70% Opacity
+
+// Sub Colors
+val Purple = Color(0xFFD1C4DA)
+val Pink = Color(0xFFFAC9C5)
+val Blue01 = Color(0xFFC8E5FF)
+val Blue02 = Color(0xFF89B2E7)
+val YellowGreen01 = Color(0xFFDEF6AB)
+val YellowGreen02 = Color(0xFFA8ECAB)
+val Yellow01 = Color(0xFFFCFAB8)
+val Yellow02 = Color(0xFFFFF2AC)
+val Orange = Color(0xFFFFD493)
+
+// Black & White
+val Black = Color(0xFF333333)
+val White = Color(0xFFFFFFFF)
+val LoginBlack = Color(0xFF000000)
+val Black20 = Color(0x33000000) // 20% Opacity
+
+// Grayscale
+val Gray01 = Color(0xFFEBEBEB)
+val Gray02 = Color(0xFFD9D9D9)
+val Gray03 = Color(0xFFB3B3B3)
+val Gray04 = Color(0xFF9E9E9E)
+val Gray05 = Color(0xFF898989)
+val Gray06 = Color(0xFFA09B9B)
+val Gray07 = Color(0xFF787878)
+
+// Caution & Delete
+val CautionRed = Color(0xFFFF8A8A)
+val Red = Color(0xFFE60000)
+
+// Kakao
+val Kakao = Color(0xFFFEE500)
