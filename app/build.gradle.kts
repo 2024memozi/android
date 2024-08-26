d4e3be4d47d8d4e1b726eb6218f047b48d962fa9
김명석 <twinaudtjr@naver.com>
2024-08-27 06:27:30 +0900

    [feat] #1 app module setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,8 @@ android {
 
 dependencies {
     implementation(projects.core.common)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.buildconfig)
     implementation(projects.core.datastore)
     implementation(projects.core.network)
-    implementation(projects.core.buildconfig)
 }
