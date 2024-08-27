fdbaef5f2fef30411d0f731e7439600baff26c91
김명석 <twinaudtjr@naver.com>
2024-08-27 15:21:01 +0900

    [chore] #1 ktlintformat

--- feature/login/src/main/java/com/memozi/login/LoginScreen.kt
+++ feature/login/src/main/java/com/memozi/login/LoginScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.memozi.designsystem.MemoziTheme
 
-
 @Composable
 fun LoginRoute(
     padding: PaddingValues,
