9efa46dca0de36fcc44d87c31124379d875fa924
김명석 <twinaudtjr@naver.com>
2024-08-31 18:21:13 +0900

    [feat] #6 MemoViewModel, MemoContract

---
+++ feature/memo/src/main/java/com/memozi/memo/MemoContract.kt
@@ -0,0 +1,14 @@
+package com.memozi.memo
+
+import com.memozi.ui.base.SideEffect
+import com.memozi.ui.base.UiState
+
+data class MemoState(
+    val nickname: String = ""
+) : UiState
+
+sealed interface MemoSideEffect : SideEffect {
+    data class NavigateToMemo(val memoId: Int) : MemoSideEffect
+    data class NavigateToCategory(val categoryId: Int) : MemoSideEffect
+    data object NavigateToSettings : MemoSideEffect
+}
