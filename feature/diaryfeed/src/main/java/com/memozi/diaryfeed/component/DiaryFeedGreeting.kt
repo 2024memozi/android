8b8f38a6a5f3bc93599b3313ca0f9664b36e9342
Minjae Son <137160756+SYAAINN@users.noreply.github.com>
2024-09-02 16:33:14 +0900

    [Chore/#3] DiaryFeedGreeting 파라미터 수정 및 AnnotatedString 적용

--- feature/diaryfeed/src/main/java/com/memozi/diaryfeed/component/DiaryFeedGreeting.kt
+++ feature/diaryfeed/src/main/java/com/memozi/diaryfeed/component/DiaryFeedGreeting.kt
@@ -11,12 +11,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.memozi.designsystem.MemoziTheme
 
 @Composable
-fun DiaryFeedGreeting() {
-    var userName by remember { mutableStateOf("홍길동") }
+fun DiaryFeedGreeting(
+    userName: String
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -24,14 +28,23 @@ fun DiaryFeedGreeting() {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = userName,
-            color = MemoziTheme.colors.white,
-            style = MemoziTheme.typography.ngBold15
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(
+                    color = MemoziTheme.colors.white,
+                    fontSize = MemoziTheme.typography.ngBold15.fontSize,
+                    fontWeight = MemoziTheme.typography.ngBold15.fontWeight)
+                ) {
+                    append(userName)
+                }
+                withStyle(style = SpanStyle(
+                    color = MemoziTheme.colors.white,
+                    fontSize = MemoziTheme.typography.ngReg13.fontSize,
+                    fontWeight = MemoziTheme.typography.ngReg13.fontWeight)
+                ) {
+                    append(" 님, 좋은 저녁이에요! 오늘 하루는 어떠셨나요?")
+                }
+            }
         )
-        Text(
-            text = " 님, 좋은 저녁이에요! 오늘 하루는 어떠셨나요?",
-            color = MemoziTheme.colors.white,
-            style = MemoziTheme.typography.ngReg13
-        )
+
     }
 }
