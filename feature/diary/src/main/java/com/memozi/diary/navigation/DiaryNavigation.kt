f41e401ff3c8ef6362941fa1164081dc8b57ff09
SYAAINN <alswo08319@gmail.com>
2024-09-08 20:14:44 +0900

    [Add/#16] Diary 모듈 navigation 생성

---
+++ feature/diary/src/main/java/com/memozi/diary/navigation/DiaryNavigation.kt
@@ -0,0 +1,27 @@
+package com.memozi.diary.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.memozi.diary.screen.DiaryScreen
+
+fun NavController.navigateDiary(navOptions: NavOptions) {
+    navigate(DiaryRoute.route, navOptions)
+}
+
+fun NavGraphBuilder.diaryNavGraph(
+    navigateToMemo: () -> Unit = {},
+    navigateToSetting: () -> Unit = {}
+) {
+    composable(route = DiaryRoute.route) {
+        DiaryScreen(
+            navigateToMemo = navigateToMemo,
+            navigateToSetting = navigateToSetting
+        )
+    }
+}
+
+object DiaryRoute {
+    const val route = "diary"
+}
