7557c513e2e1ec3c0b5f3c28adc5897e67ca41e8
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-09-27 17:56:16 +0900

    [add] #32 UserEntity

---
+++ local/user/src/main/java/com/memozi/user/di/UserLocalDataModule.kt
@@ -0,0 +1,18 @@
+package com.memozi.user.di
+
+import com.memozi.auth.source.local.UserLocalDataSource
+import com.memozi.user.datasource.UserLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class UserLocalDataModule {
+    @Binds
+    @Singleton
+    abstract fun bindsUserLocalDataSource(userLocalDataSource: UserLocalDataSourceImpl): UserLocalDataSource
+}
